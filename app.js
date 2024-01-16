c3ce603f14c01acf95f84bad8eafa67c1aab1001
Artzkaizen <arbeitchuka@gmail.com>
2024-01-16 11:24:07 +0100

    authentication res req

--- app.js
+++ app.js
@@ -10,20 +10,21 @@ app.get('/', (req, res)  => {
     res.send("Hello Chuka");
 });
 
+const timestamp = Date.now();
 app.post('/api/login', (req, res) => {
     let hasAunthenticatedUser = false;
-
     for(let i = 0; i < database.users.length; i++){
 
         const userToCheck = database.users[i];
 
         if (req.body.name === userToCheck.username && req.body.password === userToCheck.password) {
-            res.send("AUTHENTICATED!!!");
+            res.send(`${userToCheck.username}_${timestamp}`);
+            hasAunthenticatedUser = true;
             break;
         }
     }
-    if (hasAunthenticatedUser){
-        res.send("AUTHENTICATION FAILED!!!")
+    if (hasAunthenticatedUser === false){
+        res.sendStatus(401)
     }
 })
 
