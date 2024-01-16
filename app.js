fa96f2628703cbdb19d328c3b690ab75a313519c
Artzkaizen <arbeitchuka@gmail.com>
2024-01-16 14:33:14 +0100

    landing page

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const database = require("./database.js");
 const express = require("express");
+const path = require('path')
 const app = express();
 const port = 3000;
 
 // middleware
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res)  => {
-    res.send("Hello Chuka");
+    res.redirect('/home');
 });
 
-const timestamp = Date.now();
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'landing-page.html'));
+});
+
+
 app.post('/api/login', (req, res) => {
     let hasAunthenticatedUser = false;
     for(let i = 0; i < database.users.length; i++){
@@ -18,7 +23,10 @@ app.post('/api/login', (req, res) => {
         const userToCheck = database.users[i];
 
         if (req.body.name === userToCheck.username && req.body.password === userToCheck.password) {
-            res.send(`${userToCheck.username}_${timestamp}`);
+            // creates unique for session token login attempt
+            const sessionToken = `${userToCheck.username}_${Date.now()}`;
+
+            res.send(sessionToken);
             hasAunthenticatedUser = true;
             break;
         }
