df7c4ef18cf9679a42db66b81f8a02b153ee316e
Artzkaizen <arbeitchuka@gmail.com>
2024-01-25 14:39:04 +0100

    added database.js

---
+++ database.js
@@ -0,0 +1,18 @@
+const users = [{
+    username: 'Chuka',
+    password: '12345',
+    city: 'berlin',
+    profilePicture: '/img/person1.jpg'
+}, {
+    username: 'Max',
+    password: 'win1',
+    city: 'abuja',
+    profilePicture: '/img/person2.jpg'
+}, {
+    username: 'Laura',
+    password: 'yes',
+    city: 'London',
+    profilePicture: '/img/person3.jpg'
+}]
+
+module.exports = {users}
